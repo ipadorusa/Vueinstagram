19c2bc7552b402c39b565e4db5801951ab31e59e
ipadorusa <ipadorusa@gmail.com>
2017-09-27 21:22:10 +0900

    first-commit

---
+++ src/components/Feed.vue
@@ -0,0 +1,20 @@
+<template>
+  <md-card>
+    <md-card-media>
+      <img :src="imageUrl" alt="">
+    </md-card-media>
+    <md-card-header>
+      <div class="md-title">{{ full_name }}</div>
+      <div class="md-subhead">@{{ username }}</div>
+      <md-card-content>
+        {{ contents }}
+      </md-card-content>
+    </md-card-header>
+  </md-card>
+</template>
+<script>
+export default {
+  name: 'Feed',
+  props: ['imageUrl', 'full_name', 'username', 'contents']
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <md-button class="md-raised md-primary" href="https://www.instagram.com/oauth/authorize/?client_id=82a99d11d5c248298346ce2acb2aa771&redirect_uri=http://localhost:8080&response_type=token&scope=public_content">
+        인스타그램 로그인
+    </md-button>
+</div>
+</template>
+<script>
+export default {
+  name: 'Login'
+}
+</script>
